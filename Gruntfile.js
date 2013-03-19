76a384ac5725f4d11b3095ff9a095625af535abc
Maryna Aleksandrova <hello@marynaaleksandrova.info>
2013-03-19 16:05:34 -0600

    Add s3 task for grunt. Add Stylus task for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    aws: grunt.file.readJSON('grunt-aws.json'), // for S3
+
     jade: {
       compile: {
         options: {
@@ -11,12 +13,53 @@ module.exports = function(grunt) {
           }
         },
         files: {
-          "dist/index.html": ["jade/index.jade"]
+          'dist/index.html': ['jade/index.jade']
         }
       }
+    },
+
+    stylus: {
+      compile: {
+        options: {
+          paths: ['styl'], // folder, where files to be imported are located
+          urlfunc: 'url', // use embedurl('test.png') in our code to trigger Data URI embedding
+          'include css': true
+        },
+        files: {
+          'dist/index.css': 'styl/index.styl' // 1:1 compile
+        }
+      }
+    },
+
+    s3: {
+      key: '<%= aws.key %>',
+      secret: '<%= aws.secret %>',
+      bucket: '<%= aws.bucket %>',
+      access: 'public-read',
+
+      // Files to be uploaded.
+      upload: [
+        {
+          src: 'dist/index.html',
+          dest: 'index.html',
+          gzip: true
+        },
+        {
+          src: 'dist/index.css',
+          dest: 'index.css',
+          gzip: true
+        },
+        {
+          src: 'dist/robots.txt',
+          dest: 'robots.txt'
+        }
+      ]
     }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-s3');
 
 };
